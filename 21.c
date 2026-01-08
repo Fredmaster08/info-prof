491db06a2f08dd20848b1d3a2f3031e1b36fd328
Fredmaster08 <frxdmaster@gmail.com>
2026-01-08 17:37:06 +0100

    Implementation plan

---
+++ 21.c
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+#define MAX_DESC_LENGTH 256
+
+
+typedef struct {
+    int id;
+    char description[MAX_DESC_LENGTH];
+    char assignee[MAX_DESC_LENGTH]
+}
+
+    
+int main() {
+    
+    return 0;
+}
